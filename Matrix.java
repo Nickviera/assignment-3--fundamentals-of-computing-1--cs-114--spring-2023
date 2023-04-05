737cc509558b9000391f05db00b90a1ce9b22432
Nickviera <123420244+Nickviera@users.noreply.github.com>
2023-04-05 15:47:57 -0400

    assignment submition

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,80 @@
+//***************************************************************************/
+//
+//
+//     Assignment 3: Maxtrix.java   Author : Nicolas Viera
+//
+//
+//***************************************************************************** */
+
+
+import java.util.Scanner;
+
 public class Matrix {
+  private int [][]matrix;
+
+  public Matrix(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a positive integer for matrix size: ");
+    int matrixSize = scanner.nextInt();
+    matrix = new int [matrixSize][matrixSize];
+  }
+
+  public void checkPrint(){
+
+      for (int row = 0; row < matrix.length; row++) {
+        for (int col = 0; col < matrix.length; col++) {
+          matrix[row][col] = 0;
+
+        }
+      }
+  }
+
+  public void actualMatrix(){
+    int k =1;
+    for(int row = 0; row < matrix.length; row++){
+      for(int col = 0; col < matrix.length; col++){
+        matrix[row][col] = k++;
+      }
+    }
+  }
+// Method used to print Matrix and higlight diagonal numbers
+  public void printMatrix(){
+    int x1 = 0;
+    int y1 = matrix.length - 1;
+    for(int row = 0; row < matrix.length; row++) {
+      for(int col = 0; col < matrix.length; col++){
+        if(row == x1 && col == y1){
+          System.out.print("\u001B[33m");
+        }
+        System.out.printf("%4d ", matrix [row][col]);
+        System.out.print("\u001B[0m");
+      }
+      x1++;
+      y1--;
+        System.out.println();
+    }
+    System.out.println();
+    System.out.println();
+  }
+
+  private void swap(int x1, int y1, int x2, int y2){
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[y2][x2];
+    matrix[y2][x2] = temp;
+  }
+
+  public void flipMatrix() {
+    int n = matrix.length;
+    //Swap elements on one side of the diagonal with corresponding elements on the other side
+    int row;
+    int col;
+    for ( row = 0; row < n; row++) {
+      for (col = 0; col < n-1-row; col++) {
+        swap(row, col, n-1-col, n-1-row);
+
+
+      }
+    }
+  }
 }
+
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,6 +1,14 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
-public class RunMatrix {
-  public static void main(String[] args) {
+public class RunMatrix{
+  public static void main(String[]args){
+    Matrix m = new Matrix();
+
+    m. checkPrint(); // check to see with zeros if the output is correct size
+    m. printMatrix();// prints whats inside the matrix with zeros
+    m. actualMatrix();//  creates maxtix using input user input
+    m. printMatrix();// prints the original maxtrix
+    m. flipMatrix();// flips the OG matrix
+    m. printMatrix();// prints the flipped matrix
   }
 }
